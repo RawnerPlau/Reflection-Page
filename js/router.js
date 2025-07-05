bc23e68c9dc1c82f7cb34fc279933e382696e450
RawnerPlau <delosreyeswp@gmail.com>
2025-07-05 21:50:21 +0800

    created a quote form for quote inputs

--- js/router.js
+++ js/router.js
@@ -1,39 +1,39 @@
 import { renderQuote } from './quotesView.js';
 const views = {
-    home: 'views/quotes.html',
+    home: 'views/home.html',
     quotes: 'views/quotes.html',
     journal: 'views/journal.html'
   };
   
-  function loadView(viewName) {
-    fetch(views[viewName])
-      .then(res => res.text())
-      .then(html => {
-        document.getElementById('views-container').innerHTML = html;
+function loadView(viewName) {
+  fetch(views[viewName])
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('views-container').innerHTML = html;
 
-        if (viewName === 'home') {
-            renderQuote(); // Load the quote when the quote view is active
-          }
-      });
-  }
+      if (viewName === 'home') {
+          renderQuote(); // Load the quote when the quote view is active
+        }
+    });
+}
   
-  // Listen to clicks on the header nav
-  document.addEventListener('click', e => { 
-    if (e.target.matches('a[data-view]')) {
-      e.preventDefault();
-      const viewName = e.target.getAttribute('data-view');
-      if (viewName != "logout"){
-        loadView(viewName);
-      } else {
-        localStorage.setItem('isLoggedIn', 'false')
-        window.location.href = '/../views/login.html';
-      }
-      
+// Listen to clicks on the header nav
+document.addEventListener('click', e => { 
+  if (e.target.matches('a[data-view]')) {
+    e.preventDefault();
+    const viewName = e.target.getAttribute('data-view');
+    if (viewName != "logout"){
+      loadView(viewName);
+    } else {
+      localStorage.setItem('isLoggedIn', 'false')
+      window.location.href = '/../views/login.html';
     }
-  });
+      
+  }
+});
   
-  // Load default view on page load
-  window.addEventListener('DOMContentLoaded', () => {
-    loadView('home');
-  });
+// Load default view on page load
+window.addEventListener('DOMContentLoaded', () => {
+  loadView('home');
+});
   
